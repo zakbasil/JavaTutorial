c8b962543b91fb309cc98ca1a69dd2eadece12bb
zakbasil <basilskaria1729@gmail.com>
2025-07-11 12:00:09 +0530

    Fib Series Updated

--- FibSeries/src/Main.java
+++ FibSeries/src/Main.java
@@ -1,17 +1,29 @@
+import java.util.Scanner;
+
 public class Main {
-public int fib(int n) {
-    if (n==0) {
-        return(0);
+
+    static long[] fibArray;
+    public long fib(int n) {
+        if (fibArray[n] == -1) {
+            fibArray[n] = fib(n - 1) + fib(n - 2);
+        }
+        return (fibArray[n]);
     }
-    else if(n==1){
-        return(1);
-    }
-    else
-        return(fib(n-1) + fib(n-2));
-}
+
 
     public static void main(String[] args) {
         Main x = new Main();
-        System.out.println(x.fib(10));
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
+        fibArray = new long[N+1];
+        for(int i = 0; i <= N; i++)
+            fibArray[i] = -1;
+        fibArray[0] = 0;
+        fibArray[1] = 1;
+        System.out.printf("The number is %d\n",N);
+        System.out.printf("The nth term is %d\n",x.fib(N));
+        System.out.print(fibArray[0]);
+        for(int i = 1; i <= N; i++)
+            System.out.printf(" %d",fibArray[i]);
     }
 }
